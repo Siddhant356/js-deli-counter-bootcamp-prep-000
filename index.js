e6b278cdb4e9033840e3145fff3b86364ba943af
Siddhant Shukla <siddhantshukla356@gmail.com>
2019-04-30 13:55:39 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function takeANumber(line, person){
+  line.push(person)
+  return `Welcome, ${person}. You are number ${(line.length)} in line.`
+}
+function nowServing(line){
+  if (line.length!==0)
+  return `Currently serving ${line.shift()}.`
+  else
+  return  "There is nobody waiting to be served!"
+  
+}
+function currentLine(line){
+  if(line.length!==0){
+    return `The line is currently: ${line}`
+    
+  }
+  else
+  return "The line is currently empty."
+}
